5520a04e37684590efc665a55677421f781d74ed
SzymonBryniak <szymonbryniakproject@gmail.com>
2024-04-07 20:44:57 +0100

    pong game start

---
+++ paddle1.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle('square')
+        new_segment.color('green')
+        new_segment.penup()
+        new_segment.goto(position)
+        self.snake.append(new_segment)
+
