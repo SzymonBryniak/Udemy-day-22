8a80ff59da372f769d22105c02cce2fb1ccf4a7f
SzymonBryniak <szymonbryniakproject@gmail.com>
2024-06-14 22:04:41 +0100

    I want to adjust the paddle collision functions to generate more varied angles for bounce offs from the walls.

--- wall_collision.py
+++ wall_collision.py
@@ -31,5 +31,31 @@ class Wall:
             else:
                 return 330
 
+############################################################################################ I want to adjust the below functions to generate more varied angles for bounce offs from the walls
+############################################################################################
+  def bounce_1(self, range_start, range_end, ball_cor): #generates the angle value for pad2
+        print(f'bounce 1 function range:{range_start}, {range_end}')
+        default_angle = 300
 
+        for i in range(range_start, range_end + 1):
+            if default_angle == 360:
+                default_angle = 0
+            elif i == ball_cor:
+                print(f'ball coordinates pad2 = {i}, angle: {default_angle}')
+                return default_angle
+            default_angle += 2
+        print('game over')
+        return
+## I need to consider negative y coordinates of paddles
 
+    def bounce_2(self, range_start, range_end, ball_cor): #generates the angle value for pad1
+        print(f'bounce 2 function range: {range_start}, {range_end}')
+        default_angle = 250
+        for i in range(range_start, range_end + 1):
+            if i == ball_cor:
+                print(f'ball coordinates pad2 = {i}, angle: {default_angle}')
+                return default_angle
+            else:
+                default_angle -= 2
+        print('game over')
+        return
